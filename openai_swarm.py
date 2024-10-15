8ccb92b41d68e151f82ab8939f78c7ec1f671a5d
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-10-15 01:25:44 -0500

    consolidated agents and created a manager
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- openai_swarm.py
+++ openai_swarm.py
@@ -2,52 +2,62 @@ from swarm import Swarm, Agent
 
 client = Swarm()
 
+def back_to_manager():
+    return manager
+
+def transfer_to_devops():
+    return devops
+
 def transfer_to_neteng():
     return neteng
 
 def transfer_to_netsec():
     return netsec
 
-def transfer_to_infrasec():
-    return infrasec
+#def transfer_to_infrasec():
+#    return infrasec
 
 def transfer_to_appsec():
     return appsec
 
 manager = Agent(
-    name="DevOps Engineer",
-    instructions="You are the manager - assure the infrastructure code code is sent to each of the Agents and they responnd with recommendations. then pass on to the next agent.",
-    functions=[transfer_to_neteng, transfer_to_netsec, transfer_to_infrasec, transfer_to_appsec],
+    name="Manager",
+    instructions="You are the manager - introduce yourself and using the provided dockerfile give this to each agent. Once all the other agents have assest the file. Make recommendations based on what their recommendations. Assure you have feedback from all agents before providing report to user.",
+    functions=[transfer_to_devops, transfer_to_neteng, transfer_to_netsec, transfer_to_appsec],
 )
 
 devops = Agent(
     name="DevOps Engineer",
     instructions="You are a Devops Engineer, introduce yourself - assess the Dockerfile and deployment manifests and make recommendations around best practices. Then hand off to the next agent",
+    functions=[back_to_manager],
 )
 
 neteng = Agent(
     name="Network Engineer",
     instructions="You are a Network Engineer, introduce yourself - assess the Dockerfile and deployment manifests and make recommendations around best practices.",
+    functions=[back_to_manager],
 )
 
-devops = Agent(
+netsec = Agent(
     name="Network Security",
     instructions="You are a Network Security Engineer, introduce yourself - assess the Dockerfile and deployment manifests and make recommendations around best practices.",
+    functions=[back_to_manager],
 )
 
-infrasec = Agent(
-    name="Infrastructure Security Engineer",
-    instructions="You are an infrastructure security engineer, introduce yourself - assess the dockerfile and deployment manifests and make recommandations.",
-    functions=[transfer_to_appsec]
-)
+#infrasec = Agent(
+#    name="Infrastructure Security Engineer",
+#    instructions="You are an infrastructure security engineer, introduce yourself - assess the dockerfile and deployment manifests and make recommandations.",
+#    functions=[back_to_manager],
+#)
 
 appsec = Agent(
     name="Application Security Engineer",
     instructions="You are an infrastructure security engineer, introduce yourself - assess the dockerfile and deployment manifests and make recommandations",
+    functions=[back_to_manager],
 )
 
 response = client.run(
-    agent=devops,
+    agent=manager,
     messages=[{"role": "user", "content": '''
 # Use the specific Golang version with Alpine to build the application
 FROM golang:1.23.1-alpine AS builder
